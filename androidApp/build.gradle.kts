d79137c3e0f1eeea2f8551f4e29153626bccc691
SuLG-k <nenashkinvov@gmail.com>
2024-01-23 22:40:08 +0700

    add gms location

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.gms.location)
     implementation(projects.shared)
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.tooling.preview)
