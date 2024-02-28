28b2d0abd94af0c459eba52f958d11a670d1f193
SuLG-k <nenashkinvov@gmail.com>
2024-02-28 23:30:17 +0700

    feature/imagepicker: add permission request button and cameraless ability to add images from storage

--- imagepicker/src/commonMain/kotlin/ru/mystreet/imagepicker/ui/ImagePickerScreen.kt
+++ imagepicker/src/commonMain/kotlin/ru/mystreet/imagepicker/ui/ImagePickerScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Badge
@@ -23,6 +24,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -33,8 +35,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.arkivanov.decompose.extensions.compose.subscribeAsState
+import com.mohamedrejeb.calf.permissions.ExperimentalPermissionsApi
+import com.mohamedrejeb.calf.permissions.Permission
+import com.mohamedrejeb.calf.permissions.rememberPermissionState
+import com.mohamedrejeb.calf.permissions.shouldShowRationale
 import com.preat.peekaboo.image.picker.ResizeOptions
 import com.preat.peekaboo.image.picker.SelectionMode
 import com.preat.peekaboo.image.picker.rememberImagePickerLauncher
@@ -42,7 +48,6 @@ import com.preat.peekaboo.ui.CameraMode
 import com.preat.peekaboo.ui.PeekabooCamera
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import ru.mystreet.imagepicker.component.ImagePicker
 import ru.mystreet.imagepicker.domain.entity.ImageItem
 import ru.mystreet.uikit.UIKitAsyncImage
 import ru.mystreet.uikit.UIKitFilledTonalButton
@@ -51,8 +56,8 @@ import ru.mystreet.uikit.iconpack.uikiticonpack.Accept
 import ru.mystreet.uikit.iconpack.uikiticonpack.BackButton
 import ru.mystreet.uikit.iconpack.uikiticonpack.CaptureButton
 import ru.mystreet.uikit.iconpack.uikiticonpack.DiskStorage
+import ru.mystreet.uikit.iconpack.uikiticonpack.NoCamera
 import ru.mystreet.uikit.iconpack.uikiticonpack.Remove
-
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -105,13 +110,13 @@ fun ImagePickerScreen(
         PeekabooCamera(
             modifier = Modifier.fillMaxSize().padding(it),
             cameraMode = CameraMode.Back,
-            captureIcon = {
+            captureIcon = { onCapture ->
                 CaptureIconOverlay(
                     isContinueAvailable = isContinueAvailable,
-                    onCapture = it,
+                    onCapture = onCapture,
                     images = images,
                     onRemove = onRemove,
-                    onLoadFromDisk = { imagePickerLauncher.launch() },
+                    onLoadFromDisk = imagePickerLauncher::launch,
                     onAccept = { onLoad() },
                     onCancel = onBack,
                     modifier = Modifier.fillMaxSize().padding(bottom = 15.dp),
@@ -124,7 +129,12 @@ fun ImagePickerScreen(
                     }
             },
             permissionDeniedContent = {
-                Text("no permission")
+                NoCameraPermission(
+                    images = images,
+                    onRemove = onRemove,
+                    onLoadFromDisk = imagePickerLauncher::launch,
+                    modifier = Modifier.fillMaxSize()
+                )
             }
         )
     }
@@ -153,11 +163,28 @@ fun CaptureIconOverlay(
             onAccept = onAccept,
             modifier = Modifier.fillMaxWidth()
         )
-        LazyRow(
+        SelectedImagesRow(
+            images = images,
+            onRemove = onRemove,
+            onLoadFromDisk = onLoadFromDisk,
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(15.dp),
-            contentPadding = PaddingValues(horizontal = 15.dp)
-        ) {
+        )
+    }
+}
+
+@Composable
+fun SelectedImagesRow(
+    images: List<ImageItem>,
+    onRemove: (index: Int) -> Unit,
+    onLoadFromDisk: (() -> Unit)? = null,
+    modifier: Modifier = Modifier,
+) {
+    LazyRow(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(15.dp),
+        contentPadding = PaddingValues(horizontal = 15.dp)
+    ) {
+        if (onLoadFromDisk != null) {
             item(
                 contentType = { "disk_storage" }
             ) {
@@ -165,17 +192,17 @@ fun CaptureIconOverlay(
                     onLoadFromDisk = onLoadFromDisk,
                 )
             }
-            items(
-                images.size,
-                key = { images[it].uniqueId },
-                contentType = { "image" },
-            ) { index ->
-                val item = images[index]
-                Image(
-                    image = item.content,
-                    onRemove = { onRemove(index) },
-                )
-            }
+        }
+        items(
+            images.size,
+            key = { images[it].uniqueId },
+            contentType = { "image" },
+        ) { index ->
+            val item = images[index]
+            Image(
+                image = item.content,
+                onRemove = { onRemove(index) },
+            )
         }
     }
 }
@@ -298,5 +325,70 @@ fun Image(
             contentScale = ContentScale.Crop,
             modifier = Modifier.size(75.dp).clip(MaterialTheme.shapes.medium),
         )
+    }
+}
+
+@Composable
+fun NoCameraPermission(
+    images: List<ImageItem>,
+    onRemove: (index: Int) -> Unit,
+    onLoadFromDisk: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier,
+    ) {
+        CameraPermissionRequest(
+            onLoadFromDisk = onLoadFromDisk,
+            modifier = Modifier.fillMaxWidth().align(Alignment.Center),
+        )
+        SelectedImagesRow(
+            images = images,
+            onRemove = onRemove,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun CameraPermissionRequest(
+    onLoadFromDisk: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val permissionState = rememberPermissionState(Permission.Camera)
+    Column(
+        verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier,
+    ) {
+        Image(
+            UIKitIconPack.NoCamera,
+            contentDescription = null,
+            modifier = Modifier.size(150.dp)
+        )
+        Text(
+            text = "Нет доступа к камере",
+            textAlign = TextAlign.Center
+        )
+        Column {
+            OutlinedButton(
+                onClick = onLoadFromDisk,
+                modifier = Modifier.width(200.dp)
+            ) {
+                Text("Загрузить из галереи")
+            }
+            OutlinedButton(
+                onClick = {
+                    if (permissionState.status.shouldShowRationale)
+                        permissionState.launchPermissionRequest()
+                    else
+                        permissionState.openAppSettings()
+                },
+                modifier = Modifier.width(200.dp)
+            ) {
+                Text("Дать разрешение")
+            }
+        }
     }
 }
---
+++ uikit/src/commonMain/kotlin/ru/mystreet/uikit/iconpack/uikiticonpack/NoCamera.kt
@@ -0,0 +1,238 @@
+package ru.mystreet.uikit.iconpack.uikiticonpack
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType
+import androidx.compose.ui.graphics.PathFillType.Companion.NonZero
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeCap.Companion.Butt
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.ImageVector.Builder
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+import ru.mystreet.uikit.iconpack.UIKitIconPack
+
+public val UIKitIconPack.NoCamera: ImageVector
+    get() {
+        if (_noCamera != null) {
+            return _noCamera!!
+        }
+        _noCamera = Builder(name = "NoCamera", defaultWidth = 512.0.dp, defaultHeight = 512.0.dp,
+                viewportWidth = 512.0f, viewportHeight = 512.0f).apply {
+            path(fill = SolidColor(Color(0xFF596C76)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(54.122f, 150.238f)
+                curveToRelative(6.24f, 0.0f, 11.299f, -5.058f, 11.299f, -11.299f)
+                verticalLineTo(78.651f)
+                curveToRelative(0.0f, -6.24f, -5.059f, -11.299f, -11.299f, -11.299f)
+                horizontalLineTo(40.363f)
+                curveToRelative(-6.24f, 0.0f, -11.299f, 5.058f, -11.299f, 11.299f)
+                verticalLineToRelative(60.288f)
+                curveToRelative(0.0f, 6.24f, 5.059f, 11.299f, 11.299f, 11.299f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFF465A61)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(70.858f, 67.352f)
+                horizontalLineToRelative(-20.92f)
+                curveToRelative(6.24f, 0.0f, 11.299f, 5.058f, 11.299f, 11.299f)
+                verticalLineToRelative(60.288f)
+                curveToRelative(0.0f, 6.24f, -5.059f, 11.299f, -11.299f, 11.299f)
+                horizontalLineToRelative(20.92f)
+                curveToRelative(6.24f, 0.0f, 11.299f, -5.058f, 11.299f, -11.299f)
+                verticalLineTo(78.651f)
+                curveToRelative(0.0f, -6.24f, -5.059f, -11.299f, -11.299f, -11.299f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFF596C76)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveToRelative(354.027f, 108.874f)
+                lineToRelative(-28.375f, -93.026f)
+                curveTo(322.754f, 6.345f, 315.079f, 0.002f, 306.477f, 0.0f)
+                horizontalLineTo(168.335f)
+                curveToRelative(-8.604f, 0.0f, -16.281f, 6.343f, -19.18f, 15.849f)
+                lineToRelative(-28.375f, 93.026f)
+                horizontalLineToRelative(233.247f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFFD8ECFE)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveToRelative(173.817f, 59.309f)
+                lineToRelative(7.16f, -21.964f)
+                arcToRelative(4.269f, 4.269f, 0.0f, false, true, 4.058f, -2.945f)
+                horizontalLineToRelative(125.659f)
+                arcToRelative(4.27f, 4.27f, 0.0f, false, true, 4.058f, 2.945f)
+                lineToRelative(7.16f, 21.964f)
+                curveToRelative(0.899f, 2.758f, -1.157f, 5.591f, -4.058f, 5.591f)
+                horizontalLineTo(177.875f)
+                curveToRelative(-2.902f, -0.001f, -4.957f, -2.833f, -4.058f, -5.591f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFF465A61)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(346.573f, 15.849f)
+                curveTo(343.674f, 6.343f, 335.997f, 0.0f, 327.393f, 0.0f)
+                horizontalLineToRelative(-27.196f)
+                curveToRelative(8.604f, 0.0f, 16.281f, 6.343f, 19.18f, 15.849f)
+                lineToRelative(28.375f, 93.026f)
+                horizontalLineToRelative(27.197f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFF7A8C98)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(454.671f, 99.273f)
+                curveToRelative(-2.962f, -0.34f, 22.445f, -0.227f, -417.626f, -0.227f)
+                curveToRelative(-18.424f, 0.0f, -33.37f, 14.936f, -33.37f, 33.36f)
+                verticalLineToRelative(245.592f)
+                curveToRelative(0.0f, 4.566f, 0.839f, 8.267f, 2.317f, 12.213f)
+                arcToRelative(40.802f, 40.802f, 0.0f, false, false, 22.483f, 6.753f)
+                horizontalLineToRelative(413.212f)
+                curveToRelative(12.565f, 0.0f, 22.751f, -10.186f, 22.751f, -22.751f)
+                verticalLineTo(108.874f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFFD8ECFE)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(435.659f, 168.573f)
+                horizontalLineToRelative(-39.698f)
+                arcToRelative(4.268f, 4.268f, 0.0f, false, true, -4.268f, -4.268f)
+                verticalLineToRelative(-28.134f)
+                arcToRelative(4.268f, 4.268f, 0.0f, false, true, 4.268f, -4.268f)
+                horizontalLineToRelative(39.698f)
+                arcToRelative(4.268f, 4.268f, 0.0f, false, true, 4.268f, 4.268f)
+                verticalLineToRelative(28.134f)
+                arcToRelative(4.268f, 4.268f, 0.0f, false, true, -4.268f, 4.268f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFF596C76)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(454.671f, 99.273f)
+                arcToRelative(33.252f, 33.252f, 0.0f, false, true, 2.317f, 12.213f)
+                verticalLineToRelative(245.593f)
+                curveToRelative(0.0f, 18.425f, -14.936f, 33.361f, -33.36f, 33.361f)
+                horizontalLineTo(9.839f)
+                arcToRelative(33.69f, 33.69f, 0.0f, false, true, -3.847f, -0.228f)
+                curveToRelative(4.875f, 12.381f, 16.931f, 21.148f, 31.044f, 21.148f)
+                horizontalLineToRelative(413.788f)
+                curveToRelative(18.425f, 0.0f, 33.36f, -14.936f, 33.36f, -33.361f)
+                verticalLineTo(132.406f)
+                curveToRelative(0.0f, -17.123f, -12.902f, -31.225f, -29.513f, -33.133f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFF9FACBA)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(350.242f, 255.202f)
+                curveToRelative(0.0f, -72.848f, -62.092f, -122.667f, -114.93f, -122.667f)
+                curveToRelative(-62.2f, 6.289f, -110.747f, 58.808f, -110.747f, 122.667f)
+                curveToRelative(0.0f, 61.63f, 45.694f, 115.502f, 110.747f, 122.667f)
+                curveToRelative(52.897f, 0.0f, 114.93f, -49.852f, 114.93f, -122.667f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFF8D9CA8)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(235.312f, 132.535f)
+                curveToRelative(62.2f, 6.289f, 110.747f, 58.808f, 110.747f, 122.667f)
+                reflectiveCurveTo(297.512f, 371.58f, 235.312f, 377.869f)
+                curveToRelative(72.77f, 7.357f, 135.851f, -49.639f, 135.851f, -122.667f)
+                curveToRelative(0.0f, -72.81f, -62.86f, -130.047f, -135.851f, -122.667f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFF596C76)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(241.589f, 341.176f)
+                curveToRelative(42.849f, -5.185f, 76.168f, -41.749f, 76.168f, -85.974f)
+                curveToRelative(0.0f, -50.845f, -43.326f, -85.975f, -80.352f, -85.975f)
+                curveToRelative(-45.652f, 6.13f, -76.168f, 43.946f, -76.168f, 85.975f)
+                curveToRelative(0.0f, 44.225f, 33.319f, 80.791f, 76.169f, 85.974f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFF465A61)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(237.404f, 169.227f)
+                curveToRelative(42.849f, 5.184f, 76.168f, 41.75f, 76.168f, 85.975f)
+                reflectiveCurveToRelative(-33.319f, 80.79f, -76.168f, 85.974f)
+                curveToRelative(51.93f, 6.283f, 97.089f, -34.389f, 97.089f, -85.974f)
+                curveToRelative(0.0f, -51.513f, -45.089f, -92.266f, -97.089f, -85.975f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFF465A61)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(247.864f, 300.102f)
+                curveToRelative(-24.758f, 0.0f, -44.9f, -20.142f, -44.9f, -44.9f)
+                reflectiveCurveToRelative(20.142f, -44.901f, 44.9f, -44.901f)
+                reflectiveCurveToRelative(44.9f, 20.143f, 44.9f, 44.901f)
+                reflectiveCurveToRelative(-20.142f, 44.9f, -44.9f, 44.9f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFF8D9CA8)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(272.847f, 250.136f)
+                curveToRelative(-10.982f, 0.0f, -19.917f, -8.935f, -19.917f, -19.917f)
+                reflectiveCurveToRelative(8.935f, -19.917f, 19.917f, -19.917f)
+                reflectiveCurveToRelative(19.917f, 8.935f, 19.917f, 19.917f)
+                reflectiveCurveToRelative(-8.934f, 19.917f, -19.917f, 19.917f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFFFE99A0)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(487.282f, 411.518f)
+                curveToRelative(0.0f, -59.645f, -51.067f, -99.684f, -92.039f, -99.684f)
+                curveToRelative(-49.534f, 6.204f, -87.858f, 48.468f, -87.858f, 99.684f)
+                curveToRelative(0.0f, 49.106f, 35.699f, 92.643f, 87.856f, 99.684f)
+                curveToRelative(40.756f, 0.0f, 92.041f, -39.801f, 92.041f, -99.684f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFFFD8087)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(395.241f, 311.834f)
+                curveToRelative(49.534f, 6.205f, 87.857f, 48.468f, 87.857f, 99.684f)
+                reflectiveCurveToRelative(-38.323f, 93.479f, -87.857f, 99.684f)
+                curveToRelative(60.317f, 7.552f, 113.083f, -39.443f, 113.083f, -99.684f)
+                curveToRelative(0.001f, -60.271f, -52.797f, -107.232f, -113.083f, -99.684f)
+                close()
+            }
+            path(fill = SolidColor(Color(0xFFFE646F)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveToRelative(429.414f, 411.518f)
+                lineToRelative(25.457f, -25.457f)
+                curveToRelative(5.953f, -5.954f, 5.953f, -15.606f, 0.0f, -21.56f)
+                reflectiveCurveToRelative(-15.606f, -5.954f, -21.56f, 0.0f)
+                lineToRelative(-25.457f, 25.457f)
+                lineToRelative(-25.457f, -25.457f)
+                curveToRelative(-5.954f, -5.953f, -15.606f, -5.953f, -21.56f, 0.0f)
+                curveToRelative(-5.953f, 5.954f, -5.953f, 15.606f, 0.0f, 21.56f)
+                lineToRelative(25.457f, 25.457f)
+                lineToRelative(-25.457f, 25.457f)
+                curveToRelative(-5.953f, 5.954f, -5.953f, 15.606f, 0.0f, 21.56f)
+                curveToRelative(5.951f, 5.952f, 15.605f, 5.955f, 21.56f, 0.0f)
+                lineToRelative(25.457f, -25.457f)
+                lineToRelative(25.457f, 25.457f)
+                curveToRelative(5.955f, 5.954f, 15.605f, 5.953f, 21.56f, 0.0f)
+                curveToRelative(5.953f, -5.954f, 5.953f, -15.606f, 0.0f, -21.56f)
+                close()
+            }
+        }
+        .build()
+        return _noCamera!!
+    }
+
+private var _noCamera: ImageVector? = null
