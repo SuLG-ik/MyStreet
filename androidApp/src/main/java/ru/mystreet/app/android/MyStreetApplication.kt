81dfbb31bf3f8c8abc782e205c5bd4b1a427fa8e
SuLG-k <nenashkinvov@gmail.com>
2024-03-21 17:00:51 +0700

    add firebase crashlytics

--- androidApp/src/main/java/ru/mystreet/app/android/MyStreetApplication.kt
+++ androidApp/src/main/java/ru/mystreet/app/android/MyStreetApplication.kt
@@ -1,11 +1,15 @@
 package ru.mystreet.app.android
 
 import android.app.Application
+import com.google.firebase.crashlytics.ktx.crashlytics
+import com.google.firebase.ktx.BuildConfig
+import com.google.firebase.ktx.Firebase
 import ru.mystreet.app.startApp
 
 class MyStreetApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        Firebase.crashlytics.setCrashlyticsCollectionEnabled(!BuildConfig.DEBUG)
         startApp()
     }
 }
