0666ff7ddf4ba1a48fdce6232ea524590ac6ab30
SuLGik <58724087+SuLG-ik@users.noreply.github.com>
2024-03-21 18:32:25 +0700

    Merge pull request #34
    * add zoom to show alert

--- map/map/src/commonMain/kotlin/ru/mystreet/map/map/component/Map.kt
+++ map/map/src/commonMain/kotlin/ru/mystreet/map/map/component/Map.kt
@@ -1,11 +1,16 @@
 package ru.mystreet.map.map.component
 
+import com.arkivanov.decompose.value.Value
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
 import ru.mystreet.app.MapController
 import ru.mystreet.map.domain.entity.MapObjectCategory
 
 interface Map {
 
     val mapController: MapController
+
+    val isNecessaryZoomAlert: Flow<Boolean>
 
     fun setCategories(categories: List<MapObjectCategory>)
 
--- map/map/src/commonMain/kotlin/ru/mystreet/map/map/ui/MapUI.kt
+++ map/map/src/commonMain/kotlin/ru/mystreet/map/map/ui/MapUI.kt
@@ -1,6 +1,7 @@
 package ru.mystreet.map.map.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import ru.mystreet.map.map.component.Map
 
@@ -11,6 +12,7 @@ fun MapUI(
 ) {
     MapScreen(
         mapController = map.mapController,
+        isNecessaryZoomAlert = map.isNecessaryZoomAlert.collectAsState(false).value,
         onZoomIn = map::onZoomInPress,
         onZoomOut = map::onZoomOutPress,
         onFollowLocation = map::onFollowLocation,
