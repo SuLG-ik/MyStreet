3a54566ceaca76ba3eabd22789e8710f15d654b7
SuLG-k <nenashkinvov@gmail.com>
2024-03-24 23:13:02 +0700

    delete all podspecs

--- core/component/build.gradle.kts
+++ core/component/build.gradle.kts
@@ -16,19 +16,6 @@ kotlin {
     iosX64()
     iosArm64()
     iosSimulatorArm64()
-
-    cocoapods {
-        summary = "Some description for the Shared Module"
-        homepage = "Link to the Shared Module homepage"
-        version = "1.0"
-        ios.deploymentTarget = "16.0"
-        podfile = project.file("../../iosApp/Podfile")
-        framework {
-            baseName = "corecomponent"
-            isStatic = true
-        }
-    }
-    
     sourceSets {
         androidMain.dependencies {
         }
