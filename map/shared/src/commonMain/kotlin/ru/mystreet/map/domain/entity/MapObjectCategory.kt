88ae14d20fff09160f5316279214da838628b654
SuLG-k <nenashkinvov@gmail.com>
2024-04-07 20:45:38 +0700

    add installation and other categories

--- map/shared/src/commonMain/kotlin/ru/mystreet/map/domain/entity/MapObjectCategory.kt
+++ map/shared/src/commonMain/kotlin/ru/mystreet/map/domain/entity/MapObjectCategory.kt
@@ -10,30 +10,23 @@ enum class MapObjectCategory(
     val image: ImageResource,
 ) {
     Bench("bench", MR.images.map_icon_bench),
-    Playground("playground", MR.images.map_icon_playground ),
+    Playground("playground", MR.images.map_icon_playground),
     StreetLight("streetlight", MR.images.map_icon_streetlight),
     Monument("monument", MR.images.map_icon_monument),
     Fountain("fountain", MR.images.map_icon_fountain),
     Bower("bower", MR.images.map_icon_bower),
     GreenArea("green_area", MR.images.map_icon_green_area),
     PublicWC("public_wc", MR.images.map_icon_public_wc),
-    Trash("trash", MR.images.map_icon_trash);
+    Trash("trash", MR.images.map_icon_trash),
+    Installation("installation", MR.images.map_icon_trash),
+    Other("other", MR.images.map_icon_trash);
 
     companion object {
 
         fun fromId(id: String): MapObjectCategory {
-            return when (id) {
-                Bench.id -> Bench
-                Playground.id -> Playground
-                StreetLight.id -> StreetLight
-                Monument.id -> Monument
-                Fountain.id -> Fountain
-                Bower.id -> Bower
-                GreenArea.id -> GreenArea
-                PublicWC.id -> PublicWC
-                Trash.id -> Trash
-                else -> throw IllegalArgumentException("MapObjectCategory with id = $id does not exits")
-            }
+            return MapObjectCategory.entries
+                .find { id == it.id }
+                ?: throw IllegalArgumentException("MapObjectCategory with id = $id does not exits")
         }
     }
 }
