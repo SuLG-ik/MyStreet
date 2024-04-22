b7fdf3af40f035b899aa68d992777239525d15ae
SuLGik <58724087+SuLG-ik@users.noreply.github.com>
2024-04-22 08:53:49 +0700

    Merge pull request #65
    * fix take picture

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.mystreet.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
         splits {
             abi {
 
--- imagepicker/src/androidMain/kotlin/ru/mystreet/imagepicker/data/PlatformImageCompressingService.android.kt
+++ imagepicker/src/androidMain/kotlin/ru/mystreet/imagepicker/data/PlatformImageCompressingService.android.kt
@@ -8,8 +8,8 @@ import java.io.ByteArrayOutputStream
 actual class PlatformImageCompressingService : ImageCompressingService {
     override suspend fun compress(image: ByteArray): ByteArray {
         val bitmap = BitmapFactory.decodeByteArray(image, 0, image.size)
-        val factor = bitmap.width / 1000
-        val scaledBitmap = Bitmap.createScaledBitmap(bitmap, 1000, bitmap.height / factor, true)
+        val factor = bitmap.width / 1000f
+        val scaledBitmap = Bitmap.createScaledBitmap(bitmap, 1000, (bitmap.height / factor).toInt(), true)
         bitmap.recycle()
         val newImageBytes = ByteArrayOutputStream()
         scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 85, newImageBytes)
